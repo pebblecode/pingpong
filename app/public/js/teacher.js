67a0a85cfff09d47aade0d4bf70e0223b8754159
George Ornbo <george@shapeshed.com>
2014-04-14 14:38:32 +0100

    change question

--- app/public/js/teacher.js
+++ app/public/js/teacher.js
@@ -1,12 +1,14 @@
-/*global Firebase:true, Handlebars:true */
+/*global Handlebars:true */
 (function (){
   'use strict';
 
-  var myRootRef = new Firebase('https://pingpongapp.firebaseio.com/');
+  //var myRootRef = new Firebase('https://pingpongapp.firebaseio.com/');
+  var questionData;
 
   function init() {
     $.getJSON('/data/sample.json', function(dataVal) {
       var data = dataVal.qa;
+      questionData = dataVal.qa;
       var questions = [];
       for (var i = 0; i < data.length; i++) {
         questions.push(data[i].question);
@@ -20,12 +22,27 @@
 
       var qListContainer = $(".questions-list");
       qListContainer.append(qList);
+
+      $(".questions-list ul li a").click(function(e) {
+        e.preventDefault();
+        var questionIndex = $(e.target).data('id');
+        var questionTemplate = $("#question-template").html();
+        var question = Handlebars.compile(questionTemplate)({
+          question: questionData[questionIndex].question,
+          id: questionIndex
+        });
+        var questionContainer = $(".question");
+        questionContainer.empty().append(question);
+      });
+
     });
+
   }
 
   $(".start-button").click(function() {
     console.log("start");
   });
+
 
   init();
 
