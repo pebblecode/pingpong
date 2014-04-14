c0975233373ac39c259d6fef23722ffb45bbb54a
Tak Tran <contact@tutaktran.com>
2014-04-14 15:05:38 +0100

    Update question on classroom view when it changes.

--- app/public/js/classroom.js
+++ app/public/js/classroom.js
@@ -1,6 +1,22 @@
+/*global Firebase:true, Handlebars:true */
 (function (){
   'use strict';
 
-  console.log("Classroom");
+  var myRootRef = new Firebase('https://pingpongapp.firebaseio.com/');
+  var currentQuestion = myRootRef.child("currentQuestion");
+
+  currentQuestion.on('value', function(data) {
+    var q = data.val();
+    var qContainer = $(".main");
+
+    if (q) {
+      var questionTemplate = $("#question-template").html();
+      var question = Handlebars.compile(questionTemplate)(q.question);
+
+      qContainer.replaceWith(question);
+    } else {
+      qContainer.empty();
+    }
+  });
 
 })();
